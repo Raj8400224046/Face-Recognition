9b3b582b9035ce163c5987ada78b3d605b59a467
Raj <rajv8150@gmail.com>
2020-04-11 09:41:23 +0530

    myproject

---
+++ FaceRecognition.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+Raj
+
+#importing the libraries
+
+import face_recognition
+import cv2
+
+image_to_detect = cv2.imread('images/download.jpg')
+cv2.imshow("test",image_to_detect)
